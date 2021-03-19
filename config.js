fe4b667e51080e42b9194006451fde46abee93d1
indraabhushan.pr <68438304+indru-dudez@users.noreply.github.com>
2021-03-19 17:05:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  botToken: "<YOUR BOT TOKEN HERE>",
-  parseMode: "HTML",
+  botToken: "1419935389:AAGH1ed0OWppcm5zncEM6xdE7wOLaQfrfwU",
+  parseMode: "https://t.me/SI_NN_ER_LS",
   apiBaseUrl: "https://snowfl.com/CtEtDPzcfBHDNVxjLqXuTCFNgtXAOJLWLzOQT/",
 };
